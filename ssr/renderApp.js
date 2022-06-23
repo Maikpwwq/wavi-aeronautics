cbde133c07886c61bcff449af5cec8ed36890e0a
Maikpwwq <mariasf@correo.udistrital.edu.co>
2022-06-23 01:22:02 -0500

    fix: cabeceras

--- ssr/renderApp.js
+++ ssr/renderApp.js
@@ -82,6 +82,10 @@ const renderApp = (app) => {
     //   return res.redirect(redirectInfo.destination)
     // }
     // END
+    res.header("Access-Control-Allow-Origin", "http://localhost:3003");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Credentials", "true");
     const store = createStore(reducer, initialState);
     const Routing = serverRoutes;
     // We need to render app twice.
