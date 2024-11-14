cf6166d08dbddd80e0aeee90868198f426f7709c
Maikpwwq <mariasf@correo.udistrital.edu.co>
2024-11-14 13:23:09 -0500

    fix: add suspense

--- src/app/tienda/producto/page.jsx
+++ src/app/tienda/producto/page.jsx
@@ -1,7 +1,20 @@
 import ProductDetail from '@/app/tienda/components/ProductDetail'
+import React, { Suspense } from 'react'
+import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
 
 function PostPage () {
-  return <ProductDetail />
+  return (
+    <Suspense
+      fallback={
+          <Box sx={{ display: 'flex' }}>
+            <CircularProgress />
+          </Box>
+        }
+      >       
+      <ProductDetail />
+    </Suspense>
+  )
 }
 
 export default PostPage
